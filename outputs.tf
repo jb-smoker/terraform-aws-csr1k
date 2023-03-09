45347b3379921ca3a135767ce1d9bfbfcf7157a1
John Smoker <jsmoker@aviatrix.com>
2023-03-09 15:27:21 -0800

    output to eip to allow it to be addressed directly

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,19 @@
 output "csr_gi1" {
-  description = "CSR GigabitEthernet1 network insterface as an object with all of it's attributes."
+  description = "CSR GigabitEthernet1 network insterface as an object with all of its attributes."
   value       = aws_network_interface.csr_gi1
 }
 
 output "csr_gi2" {
-  description = "CSR GigabitEthernet2 network insterface as an object with all of it's attributes."
+  description = "CSR GigabitEthernet2 network insterface as an object with all of its attributes."
   value       = aws_network_interface.csr_gi2
 }
 
+output "csr_eip" {
+  description = "CSR eip with all of its attributes."
+  value       = aws_eip.this
+}
+
 output "csr_instance" {
-  description = "The created CSR instance as an object with all of it's attributes."
+  description = "The created CSR instance as an object with all of its attributes."
   value       = aws_instance.this
 }
